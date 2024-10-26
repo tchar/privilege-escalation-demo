419b6f0c215200517f116e907d385ac36a84c8fa
Tilemachos Charalampous <tilemachos.charalampous@gmail.com>
2024-10-25 20:52:29 -0700

    Add linpeas & linpeas output

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
 
-docker build -t privesc .
+docker build -t privesc:latest .
 docker rm privesc
-docker run --name privesc -it privesc
+docker run --rm --name privesc -it privesc:latest
